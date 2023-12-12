ed0c5bf5202df5ba05f3d3733561f8942e8e9c02
root <root@DESKTOP-G63RSH6>
2023-12-12 11:17:57 +0100

    reviewdraft

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,22 @@
 class ReviewsController < ApplicationController
-
-  def new
-    @booking = Booking.find(params[:booking_id])
-    @review = Review.new
-  end
+  before_action :find_unit
 
   def create
-    @booking = Booking.find(params[:booking_id])
-    @review = Review.new(review_params)
-    @review.booking = @booking
-    @review.save
+    @review = @unit.reviews.build(review_params)
+    if @review.save
+      redirect_to @unit, notice: 'Review was successfully created.'
+    else
+      render 'units/show'
+    end
   end
-
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def find_unit
+    @unit = Unit.find(params[:unit_id])
   end
 end
